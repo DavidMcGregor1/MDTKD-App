449b55c69284e65a9f1143fd76ceabc7c9af9436
david <david@plisky.net>
2022-08-27 14:16:31 +0100

    adding initial application

---
+++ MdtkdApp/MdtkdApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MdtkdApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MdtkdApp/MdtkdApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MdtkdApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
